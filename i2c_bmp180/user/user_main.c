ee63042369ecb240be6da988e2b80e9da8f6dbdb
Oleg Romanyshyn <oromanyshyn@gmail.com>
2016-04-08 01:27:00 -0400

    Added pressure sensor, some clean up

---
+++ i2c_bmp180/user/user_main.c
@@ -0,0 +1,138 @@
+#include <user_interface.h>
+#include <osapi.h>
+#include <c_types.h>
+#include <mem.h>
+#include <os_type.h>
+#include "httpclient.h"
+#include "user_config.h"
+#include "driver/i2c.h"
+#include "driver/i2c_bmp180.h"
+
+//#include "driver/uart.h"
+
+typedef enum {
+	WIFI_CONNECTING,
+	WIFI_CONNECTING_ERROR,
+	WIFI_CONNECTED,
+} tConnState;
+
+unsigned char *default_certificate;
+unsigned int default_certificate_len = 0;
+unsigned char *default_private_key;
+unsigned int default_private_key_len = 0;
+
+//extern int ets_uart_printf(const char *fmt, ...);
+
+extern uint16_t readvdd33(void);
+
+LOCAL void ICACHE_FLASH_ATTR setup_wifi_st_mode(void);
+static struct ip_info ipConfig;
+static ETSTimer WiFiLinker;
+
+int ds18b20();
+
+static ETSTimer sleep_timer;
+LOCAL void ICACHE_FLASH_ATTR sleep_cb(void *arg)
+{
+    os_timer_disarm(&sleep_timer);
+    system_deep_sleep_set_option( 1 );
+    system_deep_sleep(DATA_SEND_DELAY*1000);//second*1000*1000
+}
+
+LOCAL void ICACHE_FLASH_ATTR thingspeak_http_callback(char * response, int http_status, char * full_response)
+{
+	if (http_status == 200)
+	{
+        os_timer_disarm(&WiFiLinker);
+
+        os_timer_disarm(&sleep_timer);
+        os_timer_setfn(&sleep_timer, sleep_cb, NULL);
+        os_timer_arm(&sleep_timer, 500, 1); //5s
+	}
+}
+
+static void ICACHE_FLASH_ATTR wifi_check_ip(void *arg)
+{
+	os_timer_disarm(&WiFiLinker);
+	if (wifi_station_get_connect_status() == STATION_GOT_IP)
+	{
+        wifi_get_ip_info(STATION_IF, &ipConfig);
+        if(ipConfig.ip.addr != 0) {
+            ds18b20();
+        }
+	}
+	os_timer_setfn(&WiFiLinker, (os_timer_func_t *)wifi_check_ip, NULL);
+	os_timer_arm(&WiFiLinker, WIFI_CHECK_DELAY, 0);
+}
+
+
+LOCAL void ICACHE_FLASH_ATTR setup_wifi_st_mode(void)
+{
+	wifi_set_opmode(STATION_MODE);
+	struct station_config stconfig;
+	wifi_station_disconnect();
+	wifi_station_dhcpc_stop();
+	if(wifi_station_get_config(&stconfig))
+	{
+		os_memset(stconfig.ssid, 0, sizeof(stconfig.ssid));
+		os_memset(stconfig.password, 0, sizeof(stconfig.password));
+		os_sprintf(stconfig.ssid, "%s", WIFI_CLIENTSSID);
+		os_sprintf(stconfig.password, "%s", WIFI_CLIENTPASSWORD);
+	}
+	wifi_station_connect();
+	wifi_station_dhcpc_start();
+	wifi_station_set_auto_connect(1);
+}
+
+void user_rf_pre_init(void)
+{
+}
+
+int ICACHE_FLASH_ATTR ds18b20()
+{
+    static char http_data[256];
+    uint16 adc = 0;
+    unsigned int vdd = 0;
+
+    adc = system_adc_read();
+    vdd = readvdd33();
+
+    // Start the connection process
+    int32_t temperature;
+    int32_t pressure;
+	char buff[20];
+
+    temperature = BMP180_GetTemperature();
+    pressure = BMP180_GetPressure(OSS_0);
+
+    os_sprintf(http_data, "http://%s/update?key=%s&field1=%s&field2=%d&field3=%d&field4=%d", THINGSPEAK_SERVER, THINGSPEAK_API_KEY, BMP180_Int2String(buff, temperature), (int)(pressure/133.322368), adc, vdd);
+    http_get(http_data, "", thingspeak_http_callback);
+
+    return 1;
+}
+
+void user_init(void)
+{
+    system_set_os_print(0);
+//    uart_init(BIT_RATE_115200, BIT_RATE_115200);
+//    os_delay_us(1000);
+
+//    ets_uart_printf("\r\nBooting...\r\n");
+
+	if(wifi_get_opmode() != STATION_MODE)
+	{
+		setup_wifi_st_mode();
+	}
+	if(wifi_get_phy_mode() != PHY_MODE_11N)
+		wifi_set_phy_mode(PHY_MODE_11N);
+	if(wifi_station_get_auto_connect() == 0)
+		wifi_station_set_auto_connect(1);
+
+    BMP180_Init();
+
+	// Wait for Wi-Fi connection
+	os_timer_disarm(&WiFiLinker);
+	os_timer_setfn(&WiFiLinker, (os_timer_func_t *)wifi_check_ip, NULL);
+	os_timer_arm(&WiFiLinker, WIFI_CHECK_DELAY, 0);
+}
+
